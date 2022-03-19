50623cdbe6ce84af06ccc5d39ae039980c37b01b
kolikre <ryzhuknick@gmail.com>
2022-03-19 19:43:23 +0100

    removed config file, added __init__

--- configs/config.py
+++
@@ -1,7 +0,0 @@
-URL = 'https://ironvpn.me/?source=test'
-EMAIL = 'qa@test.com'
-VALID_CARD_DATA = {
-    'number': '4532456618142692',
-    'date': '032029',
-    'cvv': '967'
-}
--- tests/test_payment.py
+++ tests/test_payment.py
@@ -1,19 +1,27 @@
-import configs.config as config
 from pom.page_object import LandingPage
 
 
 class TestPayment:
+    URL = 'https://ironvpn.me/?source=test'
+    EMAIL = 'qa@test.com'
+    VALID_CARD_DATA = {
+        'number': '4532456618142692',
+        'date': '032029',
+        'cvv': '967'
+    }
 
     def test_positive_payment(self, setup):
         """ Positive payment test """
         driver = setup
-        driver.get(config.URL)
+        driver.get(self.URL)
         lp = LandingPage(driver)
+
         lp.openEmailModal()
-        lp.setEmail(config.EMAIL)
+        lp.setEmail(self.EMAIL)
         lp.clickPrivacyPolicyCheckbox()
         lp.clickApproveEmailButton()
         lp.openPaymentModalWindow()
-        lp.typeCreditCardDataAndPay(config.VALID_CARD_DATA)
+        lp.typeCreditCardDataAndPay(self.VALID_CARD_DATA)
         title = driver.title
+
         assert title == 'SUCCESSFUL'
